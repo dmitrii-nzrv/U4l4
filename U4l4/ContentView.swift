03f0015538f3239430a33489a3c667dfb494ea8d
Dmitrii <dmitrii.nazarov7703@gmail.com>
2024-11-11 01:00:04 +0300

    Initial Commit

---
+++ U4l4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  U4l4
+//
+//  Created by Dmitrii Nazarov on 11.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ U4l4/U4l4App.swift
@@ -0,0 +1,17 @@
+//
+//  U4l4App.swift
+//  U4l4
+//
+//  Created by Dmitrii Nazarov on 11.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct U4l4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
