dcd62b5fd462bd0fc1415bf48e5a2af84d59d7fa
Dmitrii <dmitrii.nazarov7703@gmail.com>
2024-11-13 14:37:30 +0300

    final

--- U4l4/ContentView.swift
+++ U4l4/ContentView.swift
@@ -9,16 +9,135 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack() {
+            HStack{
+                Image("header")
+                Spacer()
+            }
+            .padding(.bottom, 15)
+            
+            ZStack(alignment: .top){
+                Image("img1")
+                    .resizable()
+                    .frame(height: 300)
+                    .clipShape(.rect(cornerRadius: 20))
+                    
+                    
+                VStack(spacing: 5){
+                    Text("Новый")
+                        .foregroundStyle(.red)
+                        .font(.system(size: 12 ))
+                        .padding(.top, 25)
+                        .opacity(0.9)
+                    
+                    Text("iPhone 15 Pro")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        
+                    
+                    Text("Из края вселенной прямо в ладони")
+                        .font(.system(size: 20))
+                        .foregroundStyle(.white)
+                    
+                    Button {
+                        print("Hello, World!")
+                    }
+                    
+                    label: {
+                        Text("Купить")
+                    }
+                    .font(.system(size: 14))
+                    .foregroundColor(.black)
+                    .padding(11)
+                    .padding(.horizontal, 12)
+                    .background(Color.white)
+                    .cornerRadius(5)
+                    .padding(.top, 10)
+                }
+            }
+            .padding(.bottom, 20)
+            
+            ScrollView(.horizontal){
+                HStack{
+                    Card(image: "iphones", name: "iPhone")
+                    Card(image: "watches", name: "iPhone")
+                    Card(image: "ipads", name: "iPhone")
+                }
+            }
+            .padding(.bottom, 20)
+            
+            Footer()
         }
-        .padding()
+        .padding(20)
     }
 }
 
 #Preview {
-    ContentView()
+//    ContentView()
+//        .background(Color.white)
+    ContentView2()
+        .background(Color.white)
+        
+}
+
+struct Card: View {
+    let image: String
+    let name: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 140, height: 100)
+            
+            Text(name)
+                .font(.system(size: 14))
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 30)
+        .padding(.top, 50)
+        .background(
+            
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.white)
+                .shadow(radius: 5, x: 0, y: 0)
+                
+        )
+        .frame(width: 180, height: 250)
+    }
+}
+    
+
+struct Footer: View {
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading) {
+                Text("Оплата")
+                    .font(.system(size: 18))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                
+                Text("и доставка")
+                    .font(.system(size: 18))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+            }
+            .padding(.leading, 16)
+            .padding(.top, 10)
+            
+            Image(.footer)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 180, height: 100)
+               .padding(.trailing, 8)
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color.white)
+                .shadow(radius: 1)
+        )
+    }
 }
--- U4l4/ContentViewTwo.swift
+++ U4l4/ContentViewTwo.swift
@@ -5,4 +5,93 @@
 //  Created by Dmitrii Nazarov on 12.11.2024.
 //
 
-import Foundation
+import SwiftUI
+
+// Модель продукта
+struct Product: Identifiable {
+    let id = UUID()
+    let name: String
+    let price: String
+    let imageName: String
+}
+
+// Моковые данные
+let mockProducts = [
+    Product(name: "iPhone 14 Pro 256Gb Серебристый", price: "107 990 ₽", imageName: "card1"),
+    Product(name: "iPhone 14 Pro 512Gb Золото-зеленый", price: "119 990 ₽", imageName: "card2"),
+    Product(name: "iPhone 14 128Gb Голубой", price: "62 990 ₽", imageName: "card1"),
+    Product(name: "iPhone 14 512Gb Сияющая звезда", price: "119 990 ₽", imageName: "card2"),
+    Product(name: "iPhone 14 Pro 256Gb Серебристый", price: "107 990 ₽", imageName: "card1"),
+    Product(name: "iPhone 14 Pro 512Gb Золото-зеленый", price: "119 990 ₽", imageName: "card2"),
+    Product(name: "iPhone 14 128Gb Голубой", price: "62 990 ₽", imageName: "card1"),
+    Product(name: "iPhone 14 512Gb Сияющая звезда", price: "119 990 ₽", imageName: "card2")
+]
+
+struct ContentView2: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Image(.header)
+                Spacer()
+            }
+            
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
+                    ForEach(mockProducts) { product in
+                        ProductCard(product: product)
+                    }
+                }
+                
+                
+                //.background(Color(.systemGray6))
+            }
+        }
+        .padding(.horizontal)
+    }
+    
+}
+
+struct ProductCard: View {
+    let product: Product
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Image(product.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            //.frame(height: 150)
+            
+            
+            HStack{
+                Text(product.price)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(.red)
+                Spacer()
+                Image(systemName: "circle")
+            }
+            Text(product.name)
+                .font(.body)
+                .foregroundColor(.black)
+            
+            
+            Button(action: {
+                print("Купить \(product.name)")
+            }) {
+                Text("Купить")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(8)
+            }
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.white)
+                .shadow(color: Color.gray.opacity(0.3), radius: 8, x: 0, y: 0)
+        )
+    }
+}
